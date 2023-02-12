3cd6967ba3519bc9c14b9b0a99ab0ac084462468
Nikita Druzhinin <druzhininnikita@gmail.com>
2023-02-12 17:48:03 +0700

    second commit. Delete xml-code and use java-code only

---
+++ src/main/java/ru/druzhinin/projectone/SpringConfig.java
@@ -0,0 +1,9 @@
+package ru.druzhinin.projectone;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.druzhinin.projectone")
+public class SpringConfig {
+}
